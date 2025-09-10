259ba5ec322c42702375e36f0e84f8ad44dc92d1
Артём Игнатьев <a.ignatev@rt-solar.ru>
2025-09-10 14:18:01 +0300

    remove eslint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,28 @@
-// @ts-check
 import tsEslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 
 export default tsEslint.config(
-{
-  ignores: ['.yarn/'],
-},
+  {
+    ignores: ['.yarn/'],
+  },
   tsEslint.configs.recommended,
   {
-    files: ['**/*.{ts}'],
+    ...prettierConfig,
+    files: ['**/*.{js,ts}'],
     rules: {
       '@typescript-eslint/adjacent-overload-signatures': 'off',
-      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-empty-function': 'off',
       '@typescript-eslint/no-empty-interface': 'off',
       '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
       'no-console': 'off',
-    },
-  },
-  getPrettierLinting(),
-);
-
-function getPrettierLinting() {
-  return {
-    ...prettierConfig,
-    files: ['**/*.{js,ts}'],
-    rules: {
       ...prettierConfig.rules,
       quotes: [1, 'single', 'avoid-escape'],
       'spaced-comment': ['error', 'always'],
       curly: ['error', 'all'],
     },
-  };
-}
+  }
+);
